7d9a07ef65af511b0e031bb98b6d660afa19b23f
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-09-05 17:00:36 +0000

    up

--- main.java
+++ main.java
@@ -1,5 +1,11 @@
 class main { //クラス部分
   public static void main (String[] args) {
     System.out.println("hello Java");
+    //数値はダブルクオーテーションは不要。計算も可能。" "で囲むとそのまま出力される。
+    System.out.println(3);
+    System.out.println(5+5);
+    //文字列を足し算できる。連結できる。
+    System.out.println("5"+"6");
+    System.out.println("Let's"+"Party");
   }
 }
